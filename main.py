ca94d03b9dd3b3c3ba4434558a72efa7a1aa2897
ekrsw <eisuke.koresawa@gmail.com>
2022-11-12 15:16:53 +0900

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello')
+import pandas_datareader as pdr
+import datetime
+
+start = '2000-01-01'
+end = datetime.date.today()
+ticker = 'TSLA'
+
+df = pdr.data.DataReader(ticker, 'yahoo', start, end)
+print(df)
